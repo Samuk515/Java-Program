846771b726aef9d98aa83a6e0ee35d96bfb514cf
Samuk515 <singhsamir515@gmail.com>
2026-05-12 20:41:54 +0545

    Day 1 Completed

---
+++ Basic/Q3.java
@@ -0,0 +1,5 @@
+public class Q3 {
+    public static void main(String[] args) {
+        System.out.println("Hello everyone this is new me. \n Writing code everyday doing everythings. \nJava is divided in three form.");
+    }
+}
---
+++ Basic/Q4.java
@@ -0,0 +1,11 @@
+public class Q4 {
+    static void main(String[] args) {
+        int a= 3;
+        int b= 5;
+        System.out.println("a = " +a);
+        System.out.println("b = " +b);
+        System.out.println("Sum = "+ (a+b));
+
+
+    }
+}
