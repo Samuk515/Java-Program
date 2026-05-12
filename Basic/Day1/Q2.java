1a2017ccd0c856c0f46c55384626b51ae60de2e4
Samuk515 <singhsamir515@gmail.com>
2026-05-12 20:05:32 +0545

    Day 1

---
+++ Basic/Day1/Q2.java
@@ -0,0 +1,9 @@
+public class Q2{
+     public static void main(String[] args) {
+         System.out.println("Name: Samir Singh");
+         System.out.println("Age: 20;");
+         System.out.println("City: Kathmandu");
+         System.out.println("Samir Singh, 20, kathmandu");
+
+    }
+}
