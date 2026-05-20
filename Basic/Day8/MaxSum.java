74d63e6b967ecfd7ae96be6a5863fcd0328aa3c6
Samuk515 <singhsamir515@gmail.com>
2026-05-20 21:51:46 +0545

    day8 incompleted

---
+++ Basic/Day8/MaxSum.java
@@ -0,0 +1,20 @@
+package Day8;
+
+public class MaxSum {
+    static void main(String[] args) {
+        int[] scores = {45, 82, 67, 91, 53, 78, 60};
+        int sum = 0;
+        int max = scores[0];
+
+        System.out.println("Scores :");
+        for(int score : scores){
+            System.out.print(score + " ");
+            sum += score;
+            if( score > max) max = score;
+        }
+        System.out.println();
+        System.out.println("Sum  : " + sum);
+        System.out.println("Average:" + (double) sum / scores.lenght);
+        System.out.println("Highest:" + max);
+    }
+}
