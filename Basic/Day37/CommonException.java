dc73b85cae008687cbc27abb0faf0296535c0f02
Samuk515 <singhsamir515@gmail.com>
2026-07-22 17:33:27 +0545

    Day 37

---
+++ Basic/Day37/CommonException.java
@@ -0,0 +1,49 @@
+package Day37;
+
+import java.sql.SQLOutput;
+
+public class CommonException {
+    public static void main(String[] args) {
+        // 1. ArrayIndexOutofBoundsException
+        try {
+            int[] arr = new int[3];
+            arr[5] = 10;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Caught:" + e.getClass().getSimpleName()
+            + " - " + e.getMessage());
+        }
+
+        // 2. NullPointerException
+        try {
+            String s =  null;
+            s.length();
+        } catch (NullPointerException e) {
+            System.out.println("Caught:" + e.getClass().getSimpleName()
+            + " - " + e.getMessage());
+        }
+        // 3. NumberFormatException
+     try {
+         int x = Integer.parseInt("abc");
+     } catch (NumberFormatException e) {
+         System.out.println("Caughtz:" + e.getClass().getSimpleName()
+         + " - " + e.getMessage());
+     }
+     // 4. ArithmeticException
+        try {
+            int result = 10 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("Caught:" + e.getClass().getSimpleName()
+            + " - " + e.getMessage());
+        }
+
+        // 5. ClassCastException
+        try{
+            Object o = "text";
+            Integer i = (Integer) o;
+        } catch (ClassCastException e) {
+            System.out.println("Caught:" + e.getClass().getSimpleName()
+            + " - " + e.getMessage());
+        }
+        System.out.println("Program continues normally after all exceptions!");
+    }
+}
